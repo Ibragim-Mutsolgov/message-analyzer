13ce0fd6c4a1ac5e329ed4944a35d0198d9e41cd
grinn <grinnosse@gmail.com>
2022-10-16 22:49:28 +0300

    hometask-4

--- core/src/main/java/liga/message/messageanalyzer/core/model/MessageDto.java
+++ core/src/main/java/liga/message/messageanalyzer/core/model/MessageDto.java
@@ -2,8 +2,10 @@ package liga.message.messageanalyzer.core.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class MessageDto {
 
